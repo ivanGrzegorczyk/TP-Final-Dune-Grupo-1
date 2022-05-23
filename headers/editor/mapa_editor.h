75c8408d237b347816f22df3fa562cac08288b14
Maferep <mf.pont.tv@gmail.com>
2022-05-23 00:03:05 -0400

    centro de construccion

--- headers/editor/mapa_editor.h
+++ headers/editor/mapa_editor.h
@@ -8,7 +8,7 @@ typedef std::vector<std::vector<CeldaEditor>> matriz_t;
 typedef std::vector<CeldaEditor> fila_t;
 class MapaEditor {
     matriz_t mapa;
-    //coordenada_t ubicacion_centro_construccion;
+    coordenada_t ubicacion_centro_construccion = {-1,-1};
     public:
     MapaEditor(int filas, int columnas) {
         for(int i = 0; i < filas; i++) {
@@ -17,9 +17,26 @@ class MapaEditor {
                 CeldaEditor c({i, j});
                 fila.push_back(c);
             }
+            mapa.push_back(fila);
         }
     }
     void imprimir() {
         std::cout << "Hola soy el mapa!" << std::endl;
+    }
+    void colocar_centro_construccion(coordenada_t& coordenada) {
+        // coordenada es valida
+        // TODO sistema de propiedades mejor, clase mapa
+        coordenada_t nula = {-1, -1};
+        if(ubicacion_centro_construccion != nula) {
+            mapa[ubicacion_centro_construccion.second][ubicacion_centro_construccion.first]
+                .propiedades.clear();
+        }
+        ubicacion_centro_construccion = coordenada;
+        mapa[coordenada.second][coordenada.first]
+            .propiedades.emplace_back("centro_construccion");
+    }
+    std::string centro_construccion() {
+        return mapa[ubicacion_centro_construccion.second][ubicacion_centro_construccion.first]
+            .propiedades[0];
     }
 };
--- sources/editor/editor.cpp
+++ sources/editor/editor.cpp
@@ -3,4 +3,7 @@
 int main() {
     MapaEditor m(5,5);
     m.imprimir();
+    coordenada_t construccion = {1,3};
+    m.colocar_centro_construccion(construccion);
+    std::cout << m.centro_construccion();
 }
