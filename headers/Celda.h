57da24f9527251c40e1accff6e6f0d51bb1982bf
Maferep <mf.pont.tv@gmail.com>
2022-05-22 20:27:10 -0400

    Celda

--- headers/Celda.h
+++ headers/Celda.h
@@ -5,12 +5,16 @@
 
 typedef std::pair<int, int> coordenada_t;
 
-// Celda juego principal
+// TODO mover a su propio archivo
+/*
+Celda contiene información relevante a la lógica del juego, como el terreno.
+*/
 class Celda {
     public:
     coordenada_t id;
     Celda(coordenada_t id);
     Celda();
+    
 //    Celda(const Celda&) = delete;
 //    Celda& operator=(const Celda&) = delete;
 //
