68341219e0d1ce1c39ca7ec7d175dbd125f8968f
Maferep <mf.pont.tv@gmail.com>
2022-05-25 00:10:58 -0400

    cambiar terreno

--- sources/editor/editor.cpp
+++ sources/editor/editor.cpp
@@ -9,7 +9,10 @@ int main() {
 
     coordenada_t construccion2 = {4,1};
     m.colocar_centro_construccion(construccion2);
-    std::cout << m.centro_construccion();
+    std::cout << m.centro_construccion() << std::endl;
     std::vector<coordenada_t> celdas_montania;
+    celdas_montania.push_back({3,3});
+    celdas_montania.push_back({3,4});
     m.poner_terreno(celdas_montania, "montania");
+    m.imprimir();
 }
