1421207c21595e613e8bca36df16121c841eb8be
ivanGrzegorczyk <ivanzyk12@gmail.com>
2022-05-31 20:48:41 -0300

    Subo ejemplo de ejecución del algoritmo al main del server

--- sources/server_main.cpp
+++ sources/server_main.cpp
@@ -1,21 +1,31 @@
-#include <iostream>
-
-#include "../headers/Mapa.h"
-
-void mostrar(std::vector<Celda>& recorrido) {
-    std::cout << "RECORRIDO!" << std::endl;
-    for (const Celda& celda: recorrido)
-        std::cout << celda.id.first << " " << celda.id.second << std::endl;
-}
+#include "../headers/ServerMap.h"
 
 int main() {
-    Mapa mapa(10, 10);
-    coordenada_t inicio{1,1};
-    coordenada_t objetivo{6,6};
+    ServerMap mapa(25, 120);
 
-    std::vector<Celda> recorrido = mapa.recorrer(inicio, objetivo);
+    for (int i = 0; i < 24; i++) {
+        mapa.map[i][10].ground = 'X';
+    }
 
-    mostrar(recorrido);
+    for (int i = 1; i < 10; i++) {
+        mapa.map[i][60].ground = 'X';
+    }
+
+    for (int i = 10; i < 25; i++) {
+        mapa.map[i][80].ground = 'X';
+    }
+
+    mapa.map[18][80].ground = '.';
+
+    for (int i = 11; i < 22; i++) {
+        mapa.map[i][30].ground = 'X';
+    }
+
+    for (int i = 10; i < 119; i++) {
+        mapa.map[10][i].ground = 'X';
+    }
+
+    mapa.A_star(coordenada_t {2, 2}, coordenada_t {8, 15});
 
     return 0;
 }
