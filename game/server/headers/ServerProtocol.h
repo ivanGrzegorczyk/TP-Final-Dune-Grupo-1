f2198cc30cbb4c2a26d46006b50c49a05e05f39f
ivanGrzegorczyk <ivanzyk12@gmail.com>
2022-06-10 19:13:45 -0300

    Subo protocolo para el servidor (quizás hay que modificarlo)

--- game/server/headers/ServerProtocol.h
+++ game/server/headers/ServerProtocol.h
@@ -1,8 +1,43 @@
 #ifndef SERVERPROTOCOL_H_
 #define SERVERPROTOCOL_H_
 
-class ServerProtocol {
+#include <utility>
 
+#include "../../common/headers/Socket.h"
+#include "../../common/headers/Entity.h"
+
+#define SEARCH_PATH 1
+
+class ServerProtocol {
+private:
+    Socket socket;
+
+public:
+    ServerProtocol(Socket &&skt);
+
+    /**
+     * @brief Receives a command from the client.
+     *
+     * @return command value
+     */
+    int commandReceive();
+
+    /**
+     * @brief Receives current position of a unit and goal position.
+     * It the sets the parameters of the method values with that info.
+     *
+     * @param current
+     * @param goal
+     */
+    void requestPath(coordenada_t &current, coordenada_t &goal);
+
+    /**
+     * @brief Send the shortest path a unit hasta to take in order
+     * to move to nother location.
+     *
+     * @param path
+     */
+    void sendPath(std::stack<coordenada_t> path);
 };
 
 #endif  // SERVERPROTOCOL_H_
---
+++ game/server/sources/ServerProtocol.cpp
@@ -0,0 +1,51 @@
+#include <cstdint>
+#include <stack>
+#include <netinet/in.h>
+
+#include "../headers/ServerProtocol.h"
+
+ServerProtocol::ServerProtocol(Socket &&skt) : socket(std::move(skt)) {}
+
+int ServerProtocol::commandReceive() {
+    uint8_t command;
+    socket.recvall(&command, sizeof(command));
+    return command;
+}
+
+void ServerProtocol::requestPath
+    (coordenada_t &current, coordenada_t &goal) {
+    uint16_t current_x;
+    uint16_t current_y;
+    uint16_t goal_x;
+    uint16_t goal_y;
+
+    socket.recvall(&current_x, sizeof(current_x));
+    socket.recvall(&current_y, sizeof(current_y));
+    socket.recvall(&goal_x, sizeof(goal_x));
+    socket.recvall(&goal_y, sizeof(goal_y));
+
+    current.first = ntohs(current_x);
+    current.second = ntohs(current_y);
+    goal.first = ntohs(goal_x);
+    goal.second = ntohs(goal_y);
+}
+
+void ServerProtocol::sendPath(std::stack<coordenada_t> path) {
+    if (path.empty()) {
+        // TODO Definir caso en el que no hay camino posible
+        return;
+    } else {
+        uint8_t command = SEARCH_PATH;
+        socket.sendall(&command, sizeof(command));
+    }
+
+    while (!path.empty()) {
+        uint16_t x = path.top().first;
+        x = htons(x);
+        uint16_t y = path.top().second;
+        y = htons(y);
+        path.pop();
+        socket.sendall(&x, sizeof(x));
+        socket.sendall(&y, sizeof(y));
+    }
+}
