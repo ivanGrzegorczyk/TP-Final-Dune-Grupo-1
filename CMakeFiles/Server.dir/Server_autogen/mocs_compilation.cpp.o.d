57c3f5997688b46004ea459f44dd9eac8738ee75
Maferep <mf.pont.tv@gmail.com>
2022-06-10 19:31:33 -0400

    move headers to folder

---
+++ CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/Server.dir/Server_autogen/mocs_compilation.cpp.o: \
+ /home/osboxes/Documents/Cpp/grupal\ reclone/TP-Final-Dune-Grupo-1/Server_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
