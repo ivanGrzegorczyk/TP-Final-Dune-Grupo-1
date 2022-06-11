54b11a5efc6d074cb5dcc422f222a4758cabb940
ivanGrzegorczyk <ivanzyk12@gmail.com>
2022-06-11 00:00:48 -0300

    Agrego más métodos para el gameloop del lado del server

--- game/server/headers/ThCLient.h
+++ game/server/headers/ThCLient.h
@@ -6,21 +6,30 @@
 
 #include "../../common/headers/Thread.h"
 #include "../../common/headers/Socket.h"
+#include "ServerProtocol.h"
+#include "ServerMap.h"
+#include "../../common/headers/ProtectedQueue.h"
 
 class ThClient: public Thread {
 private:
     std::atomic<bool> keep_talking;
     std::atomic<bool> is_running;
-    Socket peer;
+    ServerProtocol protocol;
+    ProtectedQueue &protectedQueue;
+    ServerMap &map;
+
+    void searchPath();
 
 protected:
     void run() override;
 
 public:
-    ThClient(Socket &&skt);
+    ThClient(Socket &&peer, ProtectedQueue &protectedQueue, ServerMap &map);
 
     bool isDead();
     void stop();
+
+    void sendEvent(Event &event);
 
     ThClient(const ThClient&) = delete;
     ThClient& operator=(const ThClient&) = delete;
