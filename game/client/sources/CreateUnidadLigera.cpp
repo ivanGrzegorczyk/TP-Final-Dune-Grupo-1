cfbce308133f739dc90e7113fdc8891ec6427429
Daceval <daceval@fi.uba.ar>
2022-06-17 16:28:37 -0300

    cambio de nombre para la clase de infanteria ligera

--- game/client/sources/CreateUnidadLigera.cpp
+++
@@ -1,9 +0,0 @@
-#include "../headers/CreateUnidadLigera.h"
-
-std::vector<uint16_t> CreateUnidadLigera::getData() {
-    return {};
-}
-
-CreateUnidadLigera::CreateUnidadLigera() : unityId(UNIT_LIGHT_INFANTRY) {
-
-}
