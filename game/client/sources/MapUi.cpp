44a491c18652a4ca54e47bf64b32d63a104c2778
Maferep <mf.pont.tv@gmail.com>
2022-06-21 13:12:29 -0400

    fix bug

--- game/client/sources/MapUi.cpp
+++ game/client/sources/MapUi.cpp
@@ -58,11 +58,8 @@ Request* MapUi::moveCharacter(int x, int y, int playerId) {
     for (auto const& [unitId, unit] : units[playerId]) {
         request = unit->walkEvent(x, y);
         if (request != nullptr) {
-            std::cout << "Enviando..." << std::endl;
             return request;
-        } else {
-            std::cout << "Bad request" << std::endl;
-        }
+        } 
     }
     return nullptr;
 }
