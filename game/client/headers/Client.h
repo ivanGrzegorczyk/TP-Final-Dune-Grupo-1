9ae28a657fecb676907f6ddd19ecf7fd93ee482a
Federico Gomez <fedemgp@gmail.com>
2022-06-23 20:52:23 -0300

    Arreglos varios del codigo del cliente y el servidor
    * Fix del uso del chronometer en el gameloop del cliente y del server (el cliente comparaba segundos con milisegundos, y
    en el server lo simplifiqué para que no se compliquen que no les queda tiempo)
    
    * agrego manejo de excepciones en todos los hilos (muy mal que no lo hayan hecho)
    
    * agrego excepciones y shutdowns de objetos que traban algunos hilos al hacer un join
    
    El server igual está fallando al cerrar con la Q debido a un shutdown inválido. Revisar y corregir (les queda de tarea)

--- game/client/headers/Client.h
+++ game/client/headers/Client.h
@@ -7,10 +7,6 @@
 #include "MapUi.h"
 #include "Protocol.h"
 
-
-typedef std::chrono::time_point<std::chrono::system_clock> chrono;
-typedef std::chrono::duration<double, std::milli> duration;
-
 class Client {
 private:
     Protocol protocol;
@@ -22,28 +18,12 @@ private:
 
 
 public:
-    duration simDeltaTime(chrono &t1, chrono &t2) {
-        t2= std::chrono::system_clock::now();
-        auto delta= t2 - t1;
-        t1 = t2;
-
-        return delta;
-    }
-
-    void sleep(const chrono &t1, const chrono &t2, duration &delta) const {
-        delta = t2 - t1;
-        if (delta.count() < (1.0 / 30))
-            usleep((1.0 / 30) - delta.count());
-    }
-
     Client(const std::string& hostname, const std::string& servicename, Renderer &rnd);
     void run();
 
     void sendToServer();
 
     void receiveOfServer();
-
-    Request* createEvent();
 
     void ProcessInput();
 
--- game/client/sources/Client.cpp
+++ game/client/sources/Client.cpp
@@ -1,8 +1,11 @@
 #include <thread>
 #include <utility>
+#include <sstream>
 #include "SDL2pp/SDL2pp.hh"
 #include "../headers/Client.h"
 #include "../headers/CreateLightInfantry.h"
+#include "common/headers/Chronometer.h"
+#include "common/headers/Constantes.h"
 
 using namespace SDL2pp;
 
@@ -18,66 +21,64 @@ void Client::run() {
     std::thread threadSend(&Client::sendToServer, this);
 
 
-    auto t1 = std::chrono::system_clock::now();
+    Chronometer chronometer;
     while(running) {
+        chronometer.tick();
+
         ProcessInput();
         update();
         renderer();
-        auto t2 = std::chrono::system_clock::now();
-        auto delta = simDeltaTime(t1, t2);
-        sleep(t1, t2, delta);
+        uint64_t delta = chronometer.tack();
+        if (delta < GAME_LOOP_RATE)
+            usleep(GAME_LOOP_RATE - delta);
     }
+    // Si algo lanza excepción antes, leakeas estos hilos. Falta RAII en estos recursos. Que sean atributos del cliente o de otro 
+    // objeto RAII que los libere en su destructor
+    sendQueue.stop();
     threadReceive.join();
     threadSend.join();
 }
 
 void Client::ProcessInput() {
-    Request *event = createEvent();
-    if(event != nullptr) {
-        sendQueue.push(event);
-    }
-}
-
-Request* Client::createEvent() {
     SDL_Event event;
     Request* req = nullptr;
-    SDL_PollEvent(&event);
-    switch (event.type) {
-        case SDL_QUIT:
-            running = false;
-            break;
-        case SDL_KEYDOWN:
-            switch (event.key.keysym.sym) {
-                case SDLK_a:
-                    int x = -1 ,y = -1;
-                    SDL_GetMouseState(&x, &y);
-                    Request *unit = new CreateLightInfantry(x / 16, y / 16);
-                    req = unit;
-                    break;
-            }
-        case SDL_MOUSEBUTTONUP:
-            if(event.button.button ==  SDL_BUTTON_RIGHT) {
-                Request* request;
-                int x, y;
-                x = event.button.x;
-                y = event.button.y;
-                request = mapUi.moveCharacter(x / 16, y / 16, clientId);
-                req = request;
-            }
-            else if(event.button.button == SDL_BUTTON_LEFT) {
-                Request* r;
-                int x, y;
-                x = event.button.x;
-                y = event.button.y;
-                r =  mapUi.mouseEvent(x, y, clientId);
-                req = r;
+    while (running && SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                std::cout << "QUIT" << std::endl;
+                running = false;
                 break;
-            }
-        default:
-            break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_a:
+                        int x = -1 ,y = -1;
+                        SDL_GetMouseState(&x, &y);
+                        Request *unit = new CreateLightInfantry(x / 16, y / 16);
+                        sendQueue.push(unit);
+                        break;
+                }
+            case SDL_MOUSEBUTTONUP:
+                if(event.button.button ==  SDL_BUTTON_RIGHT) {
+                    Request* request;
+                    int x, y;
+                    x = event.button.x;
+                    y = event.button.y;
+                    request = mapUi.moveCharacter(x / 16, y / 16, clientId);
+                    sendQueue.push(request);
+                }
+                else if(event.button.button == SDL_BUTTON_LEFT) {
+                    Request* r;
+                    int x, y;
+                    x = event.button.x;
+                    y = event.button.y;
+                    r =  mapUi.mouseEvent(x, y, clientId);
+                    sendQueue.push(r);
+                    break;
+                }
+            default:
+                break;
+        }
     }
-
-    return req;
 }
 
 void Client::update() {
@@ -88,20 +89,34 @@ void Client::update() {
 }
 
 void Client::sendToServer() {
-    while(running) {
-        std::vector<uint16_t> data;
-        Request *event = this->sendQueue.pop();
-        if (event != nullptr) {
-            data = event->getData();
-            protocol.send(event->getCommand(), data);
+    try {
+         while(running) {
+            std::vector<uint16_t> data;
+            Request *event = this->sendQueue.pop();
+            if (event != nullptr) {
+                data = event->getData();
+                protocol.send(event->getCommand(), data);
+            }
         }
+    } catch (std::exception &e) {
+        std::ostringstream oss;
+        oss << "[sender]: " << e.what() << std::endl;
+        // Para evitar RC en el flujo de error
+        std::cout << oss.str();
     }
 }
 
 void Client::receiveOfServer() {
-    while(running) {
-        Response *response = protocol.recvResponse();
-        this->recvQueue.push(response);
+    try {
+        while(running) {
+            Response *response = protocol.recvResponse();
+            this->recvQueue.push(response);
+        }
+    } catch (std::exception &e) {
+        std::ostringstream oss;
+        oss << "[receiver]: " << e.what() << std::endl;
+        // Para evitar RC en el flujo de error
+        std::cout << oss.str();
     }
 }
 
