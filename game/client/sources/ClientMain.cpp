f77f8115aaf15c4b2500302fdc37df06faa126c0
Maferep <mf.pont.tv@gmail.com>
2022-06-23 21:34:31 -0400

    go from closing qt to opening sdl

--- game/client/sources/ClientMain.cpp
+++ game/client/sources/ClientMain.cpp
@@ -1,8 +1,26 @@
 #include "SDL2pp/SDL2pp.hh"
 #include "../headers/Client.h"
+#include "../../game/client/headers/MainWindowLobby.h"
+#include "ui_mainwindow.h"
+#include <QApplication>
 
 using namespace  SDL2pp;
 int main(int argc, char* argv[]) {
+    // Clase que contiene el loop principal
+    QApplication app(argc, argv);
+    // Instancio la ventana principal
+    MainWindow ui_window;
+    ui_window.show();
+    /*
+        * QApplication::exec inicia el event loop y se queda esperando que finalice la aplicación. Cuando el usuario
+        * realice una acción que haga un exit de QT, entonces esta función retorna (con el código de error
+        * si lo hubo) y entonces se podrá crear la ventana de SDL.
+        */
+    // exec es una función bloqueante, cuando QT finaliza su ejecución, se realiza un return con el codigo de error
+    if (app.exec()) {
+        throw std::runtime_error("La aplicación QT finalizó de forma incorrecta");
+    }
+    
     SDL sdl(SDL_INIT_VIDEO);
     Window window("Client", SDL_WINDOWPOS_UNDEFINED,
                   SDL_WINDOWPOS_UNDEFINED,640, 480, SDL_WINDOW_RESIZABLE);
