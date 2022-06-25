eabeb0018b664d95acc4745dfcfda85909604481
Daceval <daceval@fi.uba.ar>
2022-06-25 17:04:19 -0300

    agrego interface

---
+++ game/client/headers/SdlEntity.h
@@ -0,0 +1,10 @@
+#ifndef TP_FINAL_DUNE_GRUPO_1_SDLENTITY_H
+#define TP_FINAL_DUNE_GRUPO_1_SDLENTITY_H
+
+class SdlEntity {
+public:
+    virtual void render() = 0;
+    virtual void update() = 0;
+};
+
+#endif //TP_FINAL_DUNE_GRUPO_1_SDLENTITY_H
