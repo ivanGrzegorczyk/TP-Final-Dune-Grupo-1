a345e2eccf7f0fa1316913d8046c911f9fa044ea
Maferep <mf.pont.tv@gmail.com>
2022-06-28 11:00:18 -0400

    move gui to the right

--- game/client/sources/MapUi.cpp
+++ game/client/sources/MapUi.cpp
@@ -8,7 +8,7 @@ MapUi::MapUi(Renderer &renderer) :
         ground (renderer, Surface(DATA_PATH "/d2k_BLOXBASE.bmp")),
         harvester(Texture(renderer, Surface(DATA_PATH "/harvester.png"))),
         building_types(std::move(factory.createBuildingTypes(rdr))),
-        gui(Rect(400,0,100,200), building_types) {
+        gui(Rect(600,0,100,200), building_types) {
     dst.SetX(0) = dst.SetY(0);
     dst.SetW(LENGTH_TILE) = dst.SetH(LENGTH_TILE);
 }
