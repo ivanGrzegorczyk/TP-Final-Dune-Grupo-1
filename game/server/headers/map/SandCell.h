5fe391d7a0cc445ae7c18df40de8b13bb4402f81
ivanGrzegorczyk <ivanzyk12@gmail.com>
2022-06-28 19:04:39 -0300

    Agrego clases para el resto de los terrenos

--- game/server/headers/map/SandCell.h
+++ game/server/headers/map/SandCell.h
@@ -11,6 +11,8 @@ public:
     SandCell(coordenada_t coord, unsigned int spice);
     int ground() override;
     unsigned int harvest() override;
+
+    ~SandCell() override = default;
 };
 
 #endif  // SANDCELL_H_
