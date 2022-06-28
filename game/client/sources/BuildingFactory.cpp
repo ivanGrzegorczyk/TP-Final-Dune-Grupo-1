a3a54b41e666c0fcacbf6e7d905cd1edff012aa0
Maferep <mf.pont.tv@gmail.com>
2022-06-28 16:47:27 -0400

    works

--- game/client/sources/BuildingFactory.cpp
+++ game/client/sources/BuildingFactory.cpp
@@ -1,10 +1,9 @@
 #include <map>
 #include "client/headers/BuildingFactory.h"
-#include "client/headers/BarracksUi.h"
 
 // TODO this reads from the yaml instead
-std::vector<BuildingType> BuildingFactory::createBuildingTypes(Renderer &render) {
-    std::vector<BuildingType> building_types;
+std::vector<std::shared_ptr<BuildingType>>  BuildingFactory::createBuildingTypes(Renderer &render) {
+    std::vector<std::shared_ptr<BuildingType>>  building_types;
     coordenada_t size = {2,2};
     std::string texture_name = std::string("Barracks.png");
     Surface surface(std::string(DATA_PATH "/") + texture_name );
@@ -12,9 +11,12 @@ std::vector<BuildingType> BuildingFactory::createBuildingTypes(Renderer &render)
     Texture texture2(render, surface);
     Texture texture3(render, surface);
     std::string lol ("lol");
-    building_types.push_back(BuildingType(1,lol,3,4,size, std::move(texture)));
-    building_types.push_back(BuildingType(5,lol,7,8, size,  std::move(texture2)));
-    building_types.push_back(BuildingType(20,lol,40,40, size, std::move(texture3)));
+    std::shared_ptr<BuildingType> type(new BuildingType(1,lol,3,4,size, std::move(texture)));
+    building_types.push_back(type);
+    type.reset(new BuildingType(1,lol,3,4,size, std::move(texture2)));
+    building_types.push_back(type);
+    type.reset(new BuildingType(1,lol,3,4,size, std::move(texture3)));
+    building_types.push_back(type);
     return building_types;
 }
 
