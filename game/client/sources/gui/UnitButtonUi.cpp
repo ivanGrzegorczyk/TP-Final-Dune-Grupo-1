01c096784959b8dde570c45f36218d92ade82805
ivanGrzegorczyk <ivanzyk12@gmail.com>
2022-07-01 01:45:45 -0300

    Faltó subir esta clase

---
+++ game/client/sources/gui/UnitButtonUi.cpp
@@ -0,0 +1,20 @@
+#include "client/headers/gui/UnitButtonUi.h"
+#include "client/headers/gui/gui.h"
+// TODO inherit with CeldaUi
+UnitButtonUi::UnitButtonUi(std::shared_ptr<UnitType> current_unit, Rect dst, GUI* ui)
+        : current(current_unit), ui(ui) {
+    rectangle = dst;
+}
+
+void UnitButtonUi::press() {
+    std::cout << "selected building: " << current->_id << std::endl;
+    ui->setUnitToSpawn(current);
+}
+
+void UnitButtonUi::render(Renderer &rdr) {
+    Point position_texture(0,0);
+    Point size_item(70,70);
+    Rect src(position_texture, size_item);
+    rdr.Copy(current->_texture, src, rectangle);
+}
+
