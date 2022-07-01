9775510978f871ca83a705f5d039f090254673cb
Daceval <daceval@fi.uba.ar>
2022-06-30 23:45:33 -0300

    cambios en clases que heredan de request

--- game/client/sources/CreateBarracks.cpp
+++ game/client/sources/CreateBarracks.cpp
@@ -1,10 +1,10 @@
 #include "../headers/CreateBarracks.h"
 
 std::vector<uint16_t> CreateBarracks::getData() {
-    std::vector<uint16_t> data;
-    data.push_back((uint16_t) BUILDING_BARRACKS);
-    data.push_back((uint16_t)coord.first);
-    data.push_back((uint16_t)coord.second);
+    std::vector<uint16_t> data(3, 0);
+    data[0] = BUILDING_BARRACKS;
+    data[1] = coord.first;
+    data[2] = coord.second;
     return data;
 }
 
