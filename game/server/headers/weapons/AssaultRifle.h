95c491e7640fd22401ddb1174e0e0e727c99446d
ivanGrzegorczyk <ivanzyk12@gmail.com>
2022-07-02 20:46:01 -0300

    Agrego el rifle de asalto

---
+++ game/server/headers/weapons/AssaultRifle.h
@@ -0,0 +1,13 @@
+#ifndef ASSAULTRIFLE_H_
+#define ASSAULTRIFLE_H_
+
+class AssaultRifle {
+private:
+    unsigned int damage;
+    unsigned int frecuency;
+
+public:
+    AssaultRifle();
+};
+
+#endif  // ASSAULTRIFLE_H_
---
+++ game/server/sources/weapons/AssaultRifle.cpp
@@ -0,0 +1,5 @@
+#include "server/headers/weapons/AssaultRifle.h"
+#include "common/headers/Constantes.h"
+
+AssaultRifle::AssaultRifle() :
+        damage(ASSAULT_RIFLE_DAMAGE), frecuency(ASSAULT_RIFLE_FREQUENCY) {}
