4623753247208d03f359c1a00ac31ccd457514a1
ivanGrzegorczyk <ivanzyk12@gmail.com>
2022-07-02 23:33:17 -0300

    El rifle de asalto hace 1 más de daño a unidades de infantería

--- game/server/sources/weapons/AssaultRifle.cpp
+++ game/server/sources/weapons/AssaultRifle.cpp
@@ -8,8 +8,11 @@ AssaultRifle::AssaultRifle() :
 bool AssaultRifle::attack(std::shared_ptr<Unit> &enemy) {
     bool killed = false;
 
-    if (chronometer.tack() >= ASSAULT_RIFLE_FREQUENCY) {
-        killed = enemy->receiveDamage(damage);
+    if (chronometer.tack() >= frecuency) {
+        if (enemy->getType() == UNIT_LIGHT_INFANTRY || enemy->getType() == UNIT_HEAVY_INFANTRY)
+            killed = enemy->receiveDamage(damage + 1);
+        else
+            killed = enemy->receiveDamage(damage);
         chronometer.tick();
     }
 
